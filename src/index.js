c1affab9ea58c08d9331c66aaceafb494ec044f7
Paulo Valenzuela <paulovalensuela@hotmail.com>
2025-10-26 18:00:23 -0600

    Cambio del home

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {AuthProvider} from "react-oidc-context";
 import reportWebVitals from './reportWebVitals';
 
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_aU7sgfE6o",
+  client_id: "4a8s94fkhmitpvtgcnv3chi1k3",
+  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+  response_type: "code",
+  scope: "phone openid email",
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+          <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
